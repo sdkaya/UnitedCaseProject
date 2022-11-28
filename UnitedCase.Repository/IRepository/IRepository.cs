778dbb78abf28572c509a6f05d990f9f939e04b1
SEDA KAYA <sedakaya8876@gmail.com>
2022-11-28 21:55:56 +0300

    Generic Repository added

---
+++ UnitedCase.Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UnitedCase.Entity.Entities;
+
+namespace UnitedCaseProject.Repository.IRepository
+{
+    public interface IRepository<TEntity> where TEntity : AbstractEntity
+    {
+
+        Task Update(TEntity entityToUpdate);
+        Task InsertAsync(TEntity entity);
+        IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes);
+        IQueryable<TEntity> GetAllAsync();
+    }
+}
+
