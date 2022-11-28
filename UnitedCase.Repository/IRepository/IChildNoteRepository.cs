7e68ec7c902373ebf5166d360f10dbe97a9f463a
SEDA KAYA <sedakaya8876@gmail.com>
2022-11-28 22:04:21 +0300

    entity repository added

---
+++ UnitedCase.Repository/IRepository/IChildNoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitedCase.Entity.Entities;
+using UnitedCaseProject.Repository.IRepository;
+
+namespace UnitedCase.Repository.IRepository
+{
+    public interface IChildNoteRepository : IRepository<ChildNote>
+    {
+
+    }
+}
